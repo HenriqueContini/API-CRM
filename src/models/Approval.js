9c65af1af0f723e072a5993fe5ab65293cd3d286
HC Dev <81761545+HenriqueContini@users.noreply.github.com>
2022-10-12 21:56:46 -0300

    Model: Approval

---
+++ src/models/Approval.js
@@ -0,0 +1,47 @@
+import Sequelize from "sequelize";
+import db from "../config/dbconfig.js";
+import Department from "./Department.js";
+import CRM from "./CRM.js";
+import User from "./User.js";
+
+const Approval = db.define('Approval', {
+    id_aprovacao: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    crm_id: {
+        type: Sequelize.INTEGER,
+        references: {
+            model: CRM,
+            key: 'id'
+        }
+    },
+    setor: {
+        type: Sequelize.INTEGER,
+        references: {
+            model: Department,
+            key: 'cod_setor'
+        }
+    },
+    responsavel: {
+        type: Sequelize.STRING(10),
+        references: {
+            model: User,
+            key: 'matricula'
+        }
+    },
+    decisao: {
+        type: Sequelize.STRING(20),
+        default: 'Pendente'
+    },
+    comentario: {
+        type: Sequelize.TEXT
+    }
+}, {
+    tableName: 'aprovacao',
+    timestamps: false
+})
+
+export default Approval;
