7329f9b6a58de00600413b4cdf0ca3a1991629d9
HC Dev <81761545+HenriqueContini@users.noreply.github.com>
2022-10-12 21:12:42 -0300

    Model: User

---
+++ src/models/User.js
@@ -0,0 +1,29 @@
+import Sequelize from "sequelize";
+import db from "../config/dbconfig.js";
+import Department from './Department.js'
+
+const User = db.define('User', {
+    matricula: {
+        type: Sequelize.STRING(10),
+        allowNull: false,
+        primaryKey: true,
+    },
+    nome: {
+        type: Sequelize.STRING(100)
+    },
+    email: {
+        type: Sequelize.STRING(100)
+    },
+    senha: {
+        type: Sequelize.STRING(100)
+    }
+}, {
+    tableName: 'usuario',
+    timestamps: false
+})
+
+User.belongsTo(Department, {
+    foreignKey: 'setor'
+})
+
+export default User;
