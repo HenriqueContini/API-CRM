6f5a688fc3bb4eb123bdb1054d3b8d15389d3f4e
HC Dev <81761545+HenriqueContini@users.noreply.github.com>
2022-10-20 19:50:16 -0300

    Controller user CRM

--- src/controllers/CRMController.js
+++ src/controllers/CRMController.js
@@ -1,4 +1,5 @@
 import CheckVariables from '../common/CheckVariables.js';
+import db from '../config/dbconfig.js';
 import User from '../models/User.js';
 import CRM from '../models/CRM.js';
 
@@ -34,9 +35,37 @@ export default class CRMController {
                 dependencia: req.body.dependencia
             })
 
-            res.status(200).send("Criado com sucesso!");
+            res.status(200).json({error: false, msg: "Criado com sucesso!"});
         } catch(e) {
             res.status(500).json({error: true, msg: 'Erro ao criar uma CRM!'});
+        }
+    }
+
+    static async usercrm(req, res) {
+        // select id, numero_crm, max(versao), requerente, descricao, data_criacao from crms where requerente = '00002' group by numero_crm;
+
+        try {
+            const user = await User.findByPk(req.params.user);
+
+            console.log(user)
+
+            if (CheckVariables.isNullOrUndefined(user)) {
+                res.status(404).json({error: true, msg: 'Usuário não encontrado!'});
+                return;
+            }
+
+            const crms = await CRM.findAll({
+                attributes: ['id', 'numero_crm', [db.fn('MAX', db.col('versao')), 'ultima_versao'], 'requerente', 'descricao', 'data_criacao'],
+                where: {
+                    requerente: user.matricula
+                },
+                group: 'numero_crm'
+            });
+
+            res.status(200).send(crms);
+        } catch (e) {
+            console.log(e);
+            res.status(500).json({error: true, msg: 'Erro ao buscar pelas CRMs!'});
         }
     }
 }
--- src/routes/CRMRoutes.js
+++ src/routes/CRMRoutes.js
@@ -5,5 +5,6 @@ const router = express.Router();
 
 router
     .post('/crm/create-crm', CRMController.createCRM)
+    .get('/crm/usercrm/:user', CRMController.usercrm)
 
 export default router;
